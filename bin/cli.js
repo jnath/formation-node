203bcafe9c360d8e1ca44aebf478f088163b302f
Jonathan Faivre <jonathanfaivre102@msn.com>
2016-10-21 17:43:19 +0200

    seconde

---
+++ bin/cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const Bossy = require('bossy');
+const server = require('../');
+const path = require('path');
+
+var definition = {
+    p: {
+        description: 'server port',
+        alias: 'port',
+        type: 'number',
+        default: 3000
+    },
+    d: {
+        description: 'server static directory',
+        alias: 'directory',
+        type: 'string'
+    },
+    h: {
+        description: 'Show help',
+        alias: 'help',
+        type: 'boolean'
+    }
+};
+
+var args = Bossy.parse(definition);
+
+if (args instanceof Error) {
+    console.error(args.message);
+    return;
+}
+
+if (args.h || !args.d) {
+    console.log(Bossy.usage(definition, 'hello -n <name>'));
+    return;
+}
+
+let directory = path.join(process.cwd(), args.d);
+let port = args.p;
+
+server.start(directory, port, () => {
+  console.log('server start');
+});
--- index.js
+++ index.js
@@ -2,9 +2,4 @@
 
 const server = require('./lib/server.js');
 
-server.start(() => {
-  console.log('server start');
-});
-
-
 module.exports = server;
